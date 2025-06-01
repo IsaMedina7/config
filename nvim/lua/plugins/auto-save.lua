a7f7e1e51f1290c31b31628f53a5f8d4f16d59f6
mikaela <mik@gmail.com>
2025-06-01 00:08:58 -0500

    add plugin auto-save.lua

---
+++ nvim/lua/plugins/auto-save.lua
@@ -0,0 +1,33 @@
+return {
+  "brianhuster/autosave.nvim",
+  event = { "InsertLeave", "TextChanged" },
+  opts = {
+    enabled = true,
+    execution_message = {
+      enabled = true,
+      message = function()
+        return "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S")
+      end,
+      dim = 0.18,
+      cleaning_interval = 1250,
+    },
+    trigger_events = {
+      "InsertLeave",
+      "TextChanged",
+    },
+    write_all_buffers = false,
+    debounce_delay = 135,
+    conditions = {
+      exists = true,
+      modifiable = true,
+      filename_is_not = {},
+      filetype_is_not = {
+        "neo-tree",
+        "lazy",
+        "qf",
+        "help",
+        "terminal",
+      },
+    },
+  },
+}

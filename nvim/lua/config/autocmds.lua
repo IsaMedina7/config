d39d1089d2878700342eb477fd5e5ab48c60bfa2
mikaela <mik@gmail.com>
2025-06-08 17:30:55 -0500

    correct formating in obsidian

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,28 @@
+local vim = vim
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Configuración para el formateo automático de Markdown
+local augroup = vim.api.nvim_create_augroup("markdown_formatting", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "markdown.mdx" },
+  callback = function()
+    -- Establece opciones locales para archivos markdown
+    vim.opt_local.textwidth = 80
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.breakindent = true
+  end,
+  group = augroup,
+})
+
+-- Formateo automático al guardar
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.md", "*.mdx" },
+  callback = function()
+    require("conform").format({ bufnr = 0 })
+  end,
+  group = augroup,
+})
---
+++ nvim/lua/plugins/markdown-formatting.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "prettier" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "markdown", "markdown_inline" })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["markdown"] = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+      },
+      formatters = {
+        prettier = {
+          -- Configuración específica para evitar MD013
+          args = {
+            "--prose-wrap", "always",
+            "--print-width", "80",
+            "--parser", "markdown"
+          },
+        },
+      },
+    },
+  },
+}
+
